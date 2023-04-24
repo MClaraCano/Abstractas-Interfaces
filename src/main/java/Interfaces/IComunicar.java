93547c28fb5f43ae9944b1721d282530fc15ace5
Clara Cano <mclara.cano@gmail.com>
2023-04-24 19:59:55 -0300

    primer commit

---
+++ src/main/java/Interfaces/IComunicar.java
@@ -0,0 +1,14 @@
+package Interfaces;
+
+public interface IComunicar {
+/*
+    Contrato donde se especifican los COMPORTANMIENTOS
+    que deben tener las clases
+
+    Para DEFINIRLA, se debe comprender cuál o cuáles comportamientos
+    serán compartidos por objetos de distinto tipo (distintas clases)
+ */
+
+
+
+}
